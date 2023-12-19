782f70c2a3f182b0ead6a366c7b68d44000cf27f
雅基 高野 <220034@jc-21.jp>
2023-12-19 11:28:49 +0900

    完成

--- astar.cpp
+++ astar.cpp
@@ -19,13 +19,22 @@ struct Cell {
     }
 };
 
-// グリッドのサイズ
+// グリッドのサイズと方向
 const int ROW = 5;
 const int COL = 5;
+const int NUM_DIRECTIONS = 8;
 
-// グリッドの移動方向（上下左右および斜め）
 const int dr[] = { -1, 1, 0, 0, -1, -1, 1, 1 };
 const int dc[] = { 0, 0, -1, 1, -1, 1, -1, 1 };
+
+// 通行可能なセルの割合を増やすための閾値
+const double PASSABLE_THRESHOLD = 0.2;
+
+// スタートとゴールの初期位置
+const int INITIAL_START_ROW = 0;
+const int INITIAL_START_COL = 0;
+const int INITIAL_GOAL_ROW = ROW - 1;
+const int INITIAL_GOAL_COL = COL - 1;
 
 // ヒューリスティック関数（マンハッタン距離）
 int heuristic(int r, int c, int goalR, int goalC) {
@@ -33,49 +42,57 @@ int heuristic(int r, int c, int goalR, int goalC) {
 }
 
 // A*アルゴリズム
-void aStar(vector<vector<int>>& grid, int startR, int startC, int goalR, int goalC) {
-    // OpenリストとClosedリストを初期化
+void aStar(vector<vector<int>>& grid, int startR, int startC, int goalR, int goalC)
+{
     vector<vector<bool>> closed(ROW, vector<bool>(COL, false));
     auto compare = [](Cell* a, Cell* b) { return a->f > b->f; };
     priority_queue<Cell*, vector<Cell*>, decltype(compare)> open(compare);
 
-    // スタートセルを作成してOpenリストに追加
     Cell* start = new Cell(startR, startC, 0, heuristic(startR, startC, goalR, goalC));
     open.push(start);
 
-    while (!open.empty()) {
-        // Openリストから最小のf値を持つセルを取得
+    while (!open.empty()) 
+    {
         Cell* current = open.top();
         open.pop();
 
         int r = current->row;
         int c = current->col;
 
-        // ゴールに到達したら経路を出力して終了
-        if (r == goalR && c == goalC) {
+        if (r == goalR && c == goalC) 
+        {
             vector<pair<int, int>> path;
-            while (current != nullptr) {
+            while (current != nullptr) 
+            {
                 path.push_back({ current->row, current->col });
                 current = current->parent;
             }
             reverse(path.begin(), path.end());
 
             // グリッド上で最短経路を表示
-            for (int i = 0; i < ROW; ++i) {
-                for (int j = 0; j < COL; ++j) {
-                    if (i == startR && j == startC) {
+            for (int i = 0; i < ROW; ++i)
+            {
+                for (int j = 0; j < COL; ++j)
+                {
+                    if (i == startR && j == startC) 
+                    {
                         cout << " S "; //スタート
                     }
-                    else if (i == goalR && j == goalC) {
+                    else if (i == goalR && j == goalC)
+                    {
                         cout << " G "; //ゴール
                     }
-                    else if (grid[i][j] == 1) {
+                    else if (grid[i][j] == 1)
+                    {
                         cout << "■ "; //壁
                     }
-                    else {
+                    else 
+                    {
                         bool isPath = false;
-                        for (const auto& p : path) {
-                            if (p.first == i && p.second == j) {
+                        for (const auto& p : path)
+                        {
+                            if (p.first == i && p.second == j)
+                            {
                                 cout << "○ "; //最短経路の道
                                 isPath = true;
                                 break;
@@ -88,81 +105,69 @@ void aStar(vector<vector<int>>& grid, int startR, int startC, int goalR, int goa
                 }
                 cout << endl;
             }
-
             return;
         }
 
         closed[r][c] = true;
 
-        // グリッド上の隣接セルを探索
-        for (int i = 0; i < 8; ++i) {
+        for (int i = 0; i < NUM_DIRECTIONS; ++i)
+        {
             int nr = r + dr[i];
             int nc = c + dc[i];
 
-            // グリッド範囲内かつ障害物でない場合
-            if (nr >= 0 && nr < ROW && nc >= 0 && nc < COL && grid[nr][nc] == 0 && !closed[nr][nc]) {
+            if (nr >= 0 && nr < ROW && nc >= 0 && nc < COL && grid[nr][nc] == 0 && !closed[nr][nc]) 
+            {
                 int gNew = current->g + 1;
                 int hNew = heuristic(nr, nc, goalR, goalC);
                 int fNew = gNew + hNew;
 
-                // 新しいセルを作成してOpenリストに追加
                 Cell* successor = new Cell(nr, nc, gNew, hNew, current);
                 open.push(successor);
             }
         }
     }
-
-    // スタートとゴールの位置をランダムに生成した迷路上で取得
-    startR = rand() % ROW;
-    startC = rand() % COL;
-
-    do {
-        goalR = rand() % ROW;
-        goalC = rand() % COL;
-    } while ((abs(startR - goalR) + abs(startC - goalC)) < ROW / 2);
-    
-    aStar(grid, startR, startC, goalR, goalC);
 }
 
 // ランダムな迷路を生成する関数
-void generateRandomMaze(vector<vector<int>>& grid, int rows, int cols) {
+void generateRandomMaze(vector<vector<int>>& grid, int rows, int cols) 
+{
     random_device rd;
     mt19937 gen(rd());
-    uniform_int_distribution<> dis(0.0, 1.0);
+    uniform_real_distribution<> dis(0.0, 1.0);
 
     grid.clear();
-    for (int i = 0; i < rows; ++i) {
+    for (int i = 0; i < rows; ++i)
+    {
         vector<int> row;
-        for (int j = 0; j < cols; ++j) {
+        for (int j = 0; j < cols; ++j) 
+        {
             double val = dis(gen);
-            // 通行可能なセルの割合を増やす
-            if (val > 0.2) { // 通行可能なセルの割合を調整するための値（0.2）を変更
+            if (val > PASSABLE_THRESHOLD)
+            {
                 row.push_back(0); // 通行可能
             }
-            else {
+            else 
+            {
                 row.push_back(1); // 通行不可能
             }
         }
         grid.push_back(row);
     }
 
-    // スタートとゴールを通行可能なセルに設定する
-    grid[0][0] = 0; // スタート
+    grid[INITIAL_START_ROW][INITIAL_START_COL] = 0; // スタート
     grid[rows - 1][cols - 1] = 0; // ゴール
 }
 
-int main() {
-    // ランダムな迷路を生成し、その迷路を使用する
-    const int ROW = 5;
-    const int COL = 5;
+int main() 
+{
     vector<vector<int>> grid;
 
-    generateRandomMaze(grid, ROW, COL); // ランダムな迷路を生成
+    generateRandomMaze(grid, ROW, COL);
 
-
-    int startR = 0, startC = 0;
-    int goalR = ROW - 1, goalC = COL - 1;
-
+    int startR = INITIAL_START_ROW;
+    int startC = INITIAL_START_COL;
+    int goalR = INITIAL_GOAL_ROW;
+    int goalC = INITIAL_GOAL_COL;
 
     aStar(grid, startR, startC, goalR, goalC);
 
